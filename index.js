18a2c44ae1b8b1bae5c1ddc4223b9ef3661bf7c4
fongmile <70105744+fongmile@users.noreply.github.com>
2023-08-17 11:40:57 +0100

    vite template

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
 	return [
