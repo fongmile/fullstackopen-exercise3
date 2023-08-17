9f9c9ae6c339e0b3d1fae874df419f77b96957cc
fongmile <70105744+fongmile@users.noreply.github.com>
2023-08-17 22:06:37 +0100

    3.13-14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const morgan = require('morgan')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
 app.use(cors())
@@ -17,43 +19,20 @@ app.use(morgan(function (tokens, req, res) {
 	].join(' ')
 }))
 
-let persons = [
-	{
-		"id": 1,
-		"name": "Arto Hellas",
-		"number": "040-123456"
-	},
-	{
-		"id": 2,
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523"
-	},
-	{
-		"id": 3,
-		"name": "Dan Abramov",
-		"number": "12-43-234345"
-	},
-	{
-		"id": 4,
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122"
-	}
-]
-
 app.get('/info', (request, response) => {	
-	response.send('Phonebook has info for '+ persons.length +' people<br/><br/>'+new Date());
+	Person.find({}).then(persons => {
+		response.send('Phonebook has info for '+ persons.length +' people<br/><br/>'+new Date());
+	})
 })
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 app.get('/api/persons/:id', (request, response)=> {
-	const id = Number(request.params.id)
-	const person = persons.find(x => x.id === id)
-	if(person)	{
+	Person.findById(request.params.id).then(person=>{
 		response.json(person)
-	}	else {
-		response.status(404).end()
-	}
+	})
 })
 app.delete('/api/persons/:id', (request, response)=> {
 	const id = Number(request.params.id)
@@ -67,31 +46,22 @@ app.delete('/api/persons/:id', (request, response)=> {
 app.post('/api/persons', (request, response)=>{
 	const body = request.body;
 
-	if(!body.name || !body.number)	{
+	if(body.name === undefined || body.number === undefined)	{
 		return response.status(400).json({error:'data missing'})
 	}
-	if(persons.findIndex((x)=>x.name===body.name)>-1)	{
+	/* if(persons.findIndex((x)=>x.name===body.name)>-1)	{
 		return response.status(400).json({error:'name must be unique'})
-	}
-	const person = {
-		id: generateId(),
+	} */
+	const person = new Person({
 		name: body.name,
 		number: body.number
-	}
+	})
 
-	persons = persons.concat(person);
-	//console.log(person);
-	response.json(person);
+	person.save().then(savedPerson=>{
+		console.log(`${savedPerson.name} saved to phonebook`);
+		response.json(savedPerson);
+	})
 })
-const generateId = () => {
-	let id = Math.ceil(Math.random() * 9999);
-	if(persons.findIndex((x)=>x.id===id)>-1)	{
-		generateId();
-	}	else {
-		return id;
-	}
-}
-
 
 
 const PORT = process.env.PORT || 3001
