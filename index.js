fca681ba66f408a87beb4643a8f125dc5f68c01a
fongmile <70105744+fongmile@users.noreply.github.com>
2023-08-17 11:11:25 +0100

    3.11

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-
-app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
 	return [
