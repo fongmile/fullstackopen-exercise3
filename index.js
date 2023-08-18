f024f332cdd986d190e465de28363eb6ee2a1dc2
fongmile <70105744+fongmile@users.noreply.github.com>
2023-08-18 22:59:16 +0100

    3.22

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(morgan(function (tokens, req, res) {
 
 app.get('/info', (request, response) => {	
 	Person.find({}).then(persons => {
-		response.send('Phonebook has info for '+ persons.length +' people<br/><br/>'+new Date());
+		response.send('Phonebook has info for '+ persons.length +' people<br/><br/>'+new Date())
 	})
 })
 app.get('/api/persons', (request, response) => {
@@ -42,14 +42,14 @@ app.get('/api/persons/:id', (request, response, next)=> {
 })
 app.delete('/api/persons/:id', (request, response,next)=> {
 	Person.findByIdAndDelete(request.params.id)
-			.then(result=> {
-				response.status(204).end()
-			})
-			.catch(error => next(error))
+		.then(()=> {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next)=>{
-	const body = request.body;
+	const body = request.body
 
 	const person = new Person({
 		name: body.name,
@@ -57,11 +57,11 @@ app.post('/api/persons', (request, response, next)=>{
 	})
 
 	person.save()
-			.then(savedPerson=>{
-				console.log(`${savedPerson.name} saved to phonebook`);
-				response.json(savedPerson);
-			})
-			.catch(error => next(error))
+		.then(savedPerson=>{
+			console.log(`${savedPerson.name} saved to phonebook`)
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -72,10 +72,10 @@ app.put('/api/persons/:id', (request, response, next) => {
 		number: body.number
 	}
 	Person.findByIdAndUpdate(
-			request.params.id, 
-			person, 
-			{ new: true , runValidators: true, context:'query' }
-		)
+		request.params.id, 
+		person, 
+		{ new: true , runValidators: true, context:'query' }
+	)
 		.then(updatedPerson => {
 			response.json(updatedPerson)
 		})
--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to MongoDB')//, url)
 
 mongoose.connect(url)
-	.then(result => {
+	.then(() => {
 		console.log('connected to MongoDB')
 	})
 	.catch((error) => {
@@ -25,7 +25,7 @@ const personSchema = new mongoose.Schema({
 		required: true,
 		validate: {
 			validator: function (v) {
-				return /\d{2}-\d{5,}/.test(v) || /\d{3}-\d{4,}/.test(v);
+				return /\d{2}-\d{5,}/.test(v) || /\d{3}-\d{4,}/.test(v)
 			},
 			message: props => `${props.value} is not a valid phone number!`
 		}
@@ -33,7 +33,7 @@ const personSchema = new mongoose.Schema({
 })
 
 personSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
+	transform: (_document, returnedObject) => {
 		returnedObject.id = returnedObject._id.toString()
 		delete returnedObject._id
 		delete returnedObject.__v
