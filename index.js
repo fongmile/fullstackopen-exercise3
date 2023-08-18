6e6265f1b0fc77c2273a401cb461ceb2d5c65365
fongmile <70105744+fongmile@users.noreply.github.com>
2023-08-18 17:00:42 +0100

    3.15 - 3.18

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
-app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(function (tokens, req, res) {
 	return [
 		tokens.method(req, res),
@@ -29,29 +29,31 @@ app.get('/api/persons', (request, response) => {
 		response.json(persons)
 	})
 })
-app.get('/api/persons/:id', (request, response)=> {
-	Person.findById(request.params.id).then(person=>{
-		response.json(person)
-	})
+app.get('/api/persons/:id', (request, response, next)=> {
+	Person.findById(request.params.id)
+		.then(person=>{
+			if(person)	{
+				response.json(person)
+			}	else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
-app.delete('/api/persons/:id', (request, response)=> {
-	const id = Number(request.params.id)
-	persons = persons.filter(x => x.id !== id)
-	response.status(204).end()
+app.delete('/api/persons/:id', (request, response,next)=> {
+	Person.findByIdAndDelete(request.params.id)
+			.then(result=> {
+				response.status(204).end()
+			})
+			.catch(error => next(error))
 })
-/* app.get('/getid', (request, response) => {
-	const newid = generateId().toString();
-	response.send(newid);
-}) */
+
 app.post('/api/persons', (request, response)=>{
 	const body = request.body;
 
 	if(body.name === undefined || body.number === undefined)	{
 		return response.status(400).json({error:'data missing'})
 	}
-	/* if(persons.findIndex((x)=>x.name===body.name)>-1)	{
-		return response.status(400).json({error:'name must be unique'})
-	} */
 	const person = new Person({
 		name: body.name,
 		number: body.number
@@ -63,8 +65,32 @@ app.post('/api/persons', (request, response)=>{
 	})
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
 
-const PORT = process.env.PORT || 3001
+	const person = {
+		name: body.name,
+		number: body.number
+	}
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
